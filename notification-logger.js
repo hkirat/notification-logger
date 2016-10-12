2ff1f17126cc0f3c5555ceeef3b042f154542af2
Harkirat Singh <harkirat96@gmail.com>
2016-10-12 17:12:11 +0530

    Completed v1

--- notification-logger.js
+++ notification-logger.js
@@ -1,4 +1,5 @@
 (function() {
+	var isInitialized = false, _console = {};
 	Notification.requestPermission();
 	function log(body, title) {
 		title = title || "alert";
@@ -14,7 +15,48 @@
 	    });
 	  }
 	}
+
+	function genericLogger() {
+		return function (body, title) {
+			title = title || "alert";
+			if (!("Notification" in window)) {
+			    alert("This browser does not support desktop notification");
+			} else if (Notification.permission === "granted") {
+				new Notification(title ,{body: body});
+		    } else if (Notification.permission !== 'denied') {
+				Notification.requestPermission(function (permission) {
+					if (permission === "granted") {
+						new Notification('alert' ,{body: body});
+					}
+				});
+			}
+		}
+	}
+
+	function originalFnCallDecorator(fn, fnName) {
+		return function() {
+			fn.apply(this, arguments);
+			if (typeof _console[fnName] === 'function') {
+				_console[fnName].apply(console, arguments);
+			}
+		};
+	}
+
+	function destroy() {
+		isInitialized = false;
+		console.log = _console.log;
+	}
+
+	function init() {
+		if (isInitialized) { return; }
+			isInitialized = true;
+		_console.log = console.log;
+		console.log = originalFnCallDecorator(log, 'log');
+	}
+
 	window.logger = {
-		log: log
+		log: log,
+		init: init,
+		destroy:destroy
 	}
 })();
