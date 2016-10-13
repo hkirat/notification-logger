16eaf8621c817cfc048abdadc491823297d76f98
Harkirat Singh <harkirat96@gmail.com>
2016-10-13 22:50:27 +0530

    Added static icon on the website

--- notification-logger.js
+++ notification-logger.js
@@ -2,15 +2,16 @@
 	var isInitialized = false, _console = {};
 	Notification.requestPermission();
 	function log(body, title) {
+		var icon = './notifications.png'
 		title = title || "Notification";
 		if (!("Notification" in window)) {
 		    alert("This browser does not support desktop notification");
 	  	} else if (Notification.permission === "granted") {
-			new Notification(title ,{body: body});
+			new Notification(title ,{body: body, icon: icon});
 	    } else if (Notification.permission !== 'denied') {
 	    	Notification.requestPermission(function (permission) {
 				if (permission === "granted") {
-					new Notification(title ,{body: body});
+					new Notification(title ,{body: body, icon: icon});
 				}
 	    });
 	  }
